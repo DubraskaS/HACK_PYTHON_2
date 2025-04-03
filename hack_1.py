9358b0a38fc1701278be232fab5ef539363da9d0
dubraska <solorzano.dubraska@gmail.com>
2025-04-02 23:03:46 -0400

    hack 1 python main-develop

--- hack_1.py
+++ hack_1.py
@@ -9,4 +9,19 @@ text: "eq" output => "eq"
 
 def fn_hack_1(s):
     result = s
+    _ls = []
+
+    txt_ls = [result[i:i+3] for i in range(0, len(result), 3)] #hace una lista dividiendo los caracteres de "s" en grupos de tres
+                                        #secuencia de números que comienzan en 0, terminan antes de la longitud de result, y se incrementan de 3 en 3
+    
+    for txt in txt_ls: #para cada item en la lista de grupos de caracteres:
+       if len(txt) % 2 != 0:  #si la cantidad de caracteres es impar
+          content = f"{txt[0]}{txt[1].upper()}{txt[2]}"  #el del medio lo pone mayuscula
+          _ls.append(content)
+       else: #si es par
+          _ls.append(txt)    #lo deja todo igual
+    result = "".join(_ls)  #une todos los elementos de la lista _ls en una sola cadena, utilizando una cadena vacía ("") como separador
     return result
+
+#prueba interna
+print(fn_hack_1("fooziman"))
