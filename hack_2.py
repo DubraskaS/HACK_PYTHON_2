c285f5235844a64f10ee9ca35ba745da29251506
dubraska <solorzano.dubraska@gmail.com>
2025-04-02 23:10:01 -0400

    hach 2 python

--- hack_2.py
+++ hack_2.py
@@ -9,5 +9,14 @@ text: "qux" output => "qx"
 
 def fn_hack_2(s):
     result = s
-    #...
+    _ls = ""
+    vocales = "aeiou"  #string de vocales
+    for n in result:
+        if n not in vocales: #compara con el string para identificar las vocales del input
+            _ls += n
+    result = _ls
+
     return result
+
+#prueba interna
+print(fn_hack_2("fooziman"))
