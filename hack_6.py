5618e58405bbe8538db9c255df1d6b66c85d7baa
dubraska <solorzano.dubraska@gmail.com>
2025-04-09 02:22:27 -0400

    hack 6 python 2

--- hack_6.py
+++ hack_6.py
@@ -11,5 +11,28 @@ text: [] output => ["0"]
 
 def fn_hack_6(s):
     result = s
-    #...
+    replace = "-"
+    _ls = []
+    ls = ""
+    i = 0
+
+    if len(result) == 0: #si la lista de entrada esta vacia, la salida es una lista con un "0"
+        _ls.append("0")
+        result = _ls
+    elif result[0].isalpha() == False: #si el primer elemento de la lista no es una letra, se convierte en string la lista
+        ls =  ",".join(str(element) for element in result)
+        result = ls
+    else: #si se trata de una letra:
+        for n in range(0, len(result)):
+            if (n+1) % 2 == 0: #posiciones pares - 1 se reemplazan por "-" 
+                _ls.append(replace)
+            else: #posiciones impares se reemplazan por el numero de posicion + 1
+                _ls.append(f"{n+1}")
+        result = _ls
+            
     return result
+
+#Practica interna 
+res = fn_hack_6(["a","b","c","d","e"])
+res1 = fn_hack_6(["0"])
+print(type(res1), res1)
