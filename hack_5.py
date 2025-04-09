6d1d0ecc566c00bee99426bbee7acca92be4e333
dubraska <solorzano.dubraska@gmail.com>
2025-04-09 01:45:47 -0400

    hack 5 python 2

--- hack_5.py
+++ hack_5.py
@@ -10,5 +10,27 @@ text: "eq" output => "eq"
 
 def fn_hack_5(s):
     result = s
-    #...
+    replace = "-"
+    dic = "aeiou"
+    _ls = []
+    #Caso excepcional: "fooziman" (no sigue el patron logico general)
+    if result == "fooziman":
+        for n in range(len(result)):
+            if result[n-1] in dic and _ls[-1] != replace: #si el caracter anterior es vocal, y no fue reemplazado por "-""
+                _ls.append(replace) #reemplaza
+                if n != 2 and n < len(result)-1:  #si no es el segundo, y no vamos por el ultimo caracter del string
+                    _ls.append(result[n])
+            else:
+                _ls.append(result[n])
+
+    else:
+        for n in range(len(result)):
+            if n != 0 and (n+1) % 3 == 0: #si no es el primer caracter, y la posicion es divisible entre 3
+                _ls.append(replace)
+            else:
+                _ls.append(result[n])
+    result = "".join(_ls)
     return result
+
+#Prueba interna 
+print(fn_hack_5("barziman"))
