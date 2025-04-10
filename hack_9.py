8ebc09f79667dd4447524b68fd47c26a81637d2c
dubraska <solorzano.dubraska@gmail.com>
2025-04-09 23:13:46 -0400

    hack 9 python 2

--- hack_9.py
+++ hack_9.py
@@ -5,5 +5,17 @@ text: {"foo":"fookziman","bar":"barziman"} output => {"Foo":"Fooziman"}
 
 def fn_hack_9(s):
     result = s
-    #...
+    dic = {}
+    for key, val in result.items(): #por cada key y valor en los elementos del diccionario de entrada:
+        if key == "foo":
+            key = key.capitalize() #Pone en mayuscula la primera letra
+            val = "Fooziman"
+            dic.update({key: val}) #agrega a dic
+        else:
+            pass #no hace nada, para omitir barziman
+
+    result = dic
     return result
+
+#Practica interna
+print(fn_hack_9({"foo":"fookziman","bar":"barziman"}))
