c192624ac99042c67adde362d0424e142e5cfdf3
dubraska <solorzano.dubraska@gmail.com>
2025-04-09 22:01:43 -0400

    hack 7 python 2

--- hack_7.py
+++ hack_7.py
@@ -11,5 +11,20 @@ text: [0] output => [0]
 
 def fn_hack_7(s):
     result = s
-    #...
+    dic = {"a": "1",
+           "b": 2,
+           "c": "3",
+           "d": 4,
+           "e": "5"}
+    _ls = []
+    for n in range(0,len(result)):
+        if result[n] in dic:
+            _ls.append(dic[result[n]]) #se aplica el diccionario
+        else:
+            _ls.append(result[n])  #se deja el mismo valor (caso 0)
+    result = _ls
+
     return result
+
+#Practica interna
+print(fn_hack_7(["a","b","c","d","e"]))
